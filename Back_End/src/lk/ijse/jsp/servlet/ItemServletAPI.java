7f9a5908f98c002cb6f672ef4aba9343cdc8bbcb
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-08-28 10:23:58 +0530

    Item crud done

--- Back_End/src/lk/ijse/jsp/servlet/ItemServletAPI.java
+++ Back_End/src/lk/ijse/jsp/servlet/ItemServletAPI.java
@@ -2,9 +2,7 @@ package lk.ijse.jsp.servlet;
 
 import lk.ijse.jsp.dto.ItemDTO;
 
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
+import javax.json.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -134,59 +132,113 @@ public class ItemServletAPI extends HttpServlet {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "1234");
-            switch (option) {
-                case "add":
-                    PreparedStatement pstm = connection.prepareStatement("insert into item values(?,?,?,?)");
-                    pstm.setObject(1, code);
-                    pstm.setObject(2, itemName);
-                    pstm.setObject(3, qtyOnHand);
-                    pstm.setObject(4, unitPrice);
-                    resp.addHeader("content-type","application/json");
-                    if (pstm.executeUpdate() > 0) {
-                        //resp.getWriter().println("Item Added..!");
-                        //resp.sendRedirect("item");
-                        JsonObjectBuilder objectItemBuilder = Json.createObjectBuilder();
-                        objectItemBuilder.add("state", "Ok");
-                        objectItemBuilder.add("message", "Successfully Added...!");
-                        objectItemBuilder.add("data", "");
-                        resp.getWriter().print(objectItemBuilder.build());
-                    }
-                    break;
-                case "delete":
-                    PreparedStatement pstm2 = connection.prepareStatement("delete from item where code=?");
-                    pstm2.setObject(1, code);
-                    resp.addHeader("content-type","application/json");
-                    if (pstm2.executeUpdate() > 0) {
-                        //resp.getWriter().println("Item Deleted..!");
-                        JsonObjectBuilder deleteObject = Json.createObjectBuilder();
-                        deleteObject.add("state", "Ok");
-                        deleteObject.add("message", "Successfully deleted...!");
-                        deleteObject.add("data", "");
-                        resp.getWriter().print(deleteObject.build());
-                        //resp.sendRedirect("/jsonp/pages/item.html");
-                    }
-                    break;
-                case "update":
-                    PreparedStatement pstm3 = connection.prepareStatement("update Item set description=?,qtyOnHand=?,unitPrice=? where code=?");
-                    pstm3.setObject(1, itemName);
-                    pstm3.setObject(2, qtyOnHand);
-                    pstm3.setObject(3, unitPrice);
-                    pstm3.setObject(4, code);
-                    if (pstm3.executeUpdate() > 0) {
-                        resp.getWriter().println("Item Updated..!");
-                        resp.sendRedirect("/jsonp/pages/item.html");
-                    }
-                    break;
+            PreparedStatement pstm = connection.prepareStatement("insert into item values(?,?,?,?)");
+            pstm.setObject(1, code);
+            pstm.setObject(2, itemName);
+            pstm.setObject(3, qtyOnHand);
+            pstm.setObject(4, unitPrice);
+
+            if (pstm.executeUpdate() > 0) {
+                JsonObjectBuilder responseObj = Json.createObjectBuilder();
+                responseObj.add("Status", "ok");
+                responseObj.add("message", "Successfully Added...!");
+                responseObj.add("data", "");
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.getWriter().print(responseObj.build());
+
+
             }
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
             JsonObjectBuilder response = Json.createObjectBuilder();
             response.add("state", "Error");
             response.add("message", e.getMessage());
             response.add("data", "");
             resp.getWriter().print(response.build());
-            //throw new RuntimeException(e);
         }
     }
-}
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Content-Type", "application/json");
+        String code = req.getParameter("code");
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/AssCompany", "root", "1234");
+            PreparedStatement pstm2 = connection.prepareStatement("delete from item where code=?");
+            pstm2.setObject(1, code);
+            if (pstm2.executeUpdate() > 0) {
+                JsonObjectBuilder responseObj = Json.createObjectBuilder();
+                responseObj.add("Status","ok");
+                responseObj.add("message","Successfully Deleted...!");
+                responseObj.add("data","");
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.getWriter().print(responseObj.build());
+
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("Status","Error");
+            error.add("message",e.getLocalizedMessage());
+            error.add("data","");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().print(error.build());
+
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject cusObj = reader.readObject();
+        String code = cusObj.getString("code");
+        String name = cusObj.getString("name");
+        String qty = cusObj.getString("qtyOnHand");
+        String price = cusObj.getString("price");
+
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/AssCompany", "root", "1234");
+            PreparedStatement pstm3 = connection.prepareStatement("update item set  name=?,qtyOnHand=?,price=? where code=?");
+            pstm3.setObject(4, code);
+            pstm3.setObject(1, name);
+            pstm3.setObject(2, qty);
+            pstm3.setObject(3, price);
+
+
+            if (pstm3.executeUpdate() > 0) {
+                JsonObjectBuilder responseObj = Json.createObjectBuilder();
+                responseObj.add("Status","ok");
+                responseObj.add("message","Successfully Updated...!");
+                responseObj.add("data","");
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.getWriter().print(responseObj.build());
+            }
+
+        } catch (SQLException | ClassNotFoundException e){
+            JsonObjectBuilder error = Json.createObjectBuilder();
+            error.add("Status","Error");
+            error.add("message",e.getLocalizedMessage());
+            error.add("data","");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().print(error.build());
+
+
+
+        }
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods","PUT,DELETE");
+        resp.addHeader("Access-Control-Allow-Headers","content-type");
+
+    }
+    }
